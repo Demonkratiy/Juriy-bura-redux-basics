2a082acd6d9468ceebc56b65f18297fd5bc23a69
Aleksei Semenov <aleksei.semenov@akvelon.com>
2020-02-19 19:55:48 +0300

    trn complete

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,14 @@
 export const inc = () => {
-  return {type: 'INC'};
-}
+  return { type: "INC" };
+};
 
-export const dec = () => ({type: 'DEC'});
+export const dec = () => ({ type: "DEC" });
 
-export const rnd = (payload) => ({type: 'RND', payload});
+//export const rnd = payload => ({ type: "RND", payload }); //payload - просто типичное имя для доп.параметров
+
+export const rnd = () => {
+  return {
+    type: "RND",
+    payload: Math.floor(Math.random() * 10)
+  };
+};
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import { connect } from 'react-redux'
-import { inc, dec, rnd } from '../actions'
+import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-const Counter = ({counter, inc, dec, rnd}) => {
+const Counter = ({ counter, inc, dec, rnd }) => {
   return (
-    <div className='jumbotron'>
+    <div className="jumbotron">
       <h2>{counter}</h2>
-      <button
-      onClick={dec}
-        className="btn btn-primary btn-lg">DEC</button>
-      <button
-        onClick={inc}
-        className="btn btn-primary btn-lg">INC</button>
-      <button
-        onClick={rnd}
-        className="btn btn-primary btn-lg">RND</button>
+      <button onClick={dec} className="btn btn-danger btn-lg">
+        DEC
+      </button>
+      <button onClick={inc} className="btn btn-primary btn-lg">
+        INC
+      </button>
+      <button onClick={rnd} className="btn btn-warning btn-lg">
+        RND
+      </button>
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     counter: state
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    inc: () => dispatch({type: 'INC'})
-  }
-}
+export default connect(
+  mapStateToProps,
+  actions
+)(Counter);
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter)
+/*const mapDispatchToProps = dispatch => {
+  return bindActionCreators(actions, dispatch);
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);*/
